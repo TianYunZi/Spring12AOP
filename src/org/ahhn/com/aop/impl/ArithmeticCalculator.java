acc91b8a296061622308e155e665cb001b390a0f
夜凡道 <zxysegh@gmail.com>
2016-02-29 23:06:30 +0800

    添加aop.impl包

---
+++ src/org/ahhn/com/aop/impl/ArithmeticCalculator.java
@@ -0,0 +1,14 @@
+package org.ahhn.com.aop.impl;
+
+/**
+ * Created by XJX on 2016/2/29.
+ */
+public interface ArithmeticCalculator {
+	int add(int i, int j);
+
+	int sub(int i, int j);
+
+	int mul(int i, int j);
+
+	int div(int i, int j);
+}
---
+++ src/org/ahhn/com/aop/impl/ArithmeticCalculatorImpl.java
@@ -0,0 +1,30 @@
+package org.ahhn.com.aop.impl;
+
+/**
+ * Created by XJX on 2016/2/29.
+ */
+public class ArithmeticCalculatorImpl implements ArithmeticCalculator {
+	@Override
+	public int add(int i, int j) {
+		int result = i + j;
+		return result;
+	}
+
+	@Override
+	public int sub(int i, int j) {
+		int result = i - j;
+		return result;
+	}
+
+	@Override
+	public int mul(int i, int j) {
+		int result = i * j;
+		return result;
+	}
+
+	@Override
+	public int div(int i, int j) {
+		int result = i / j;
+		return result;
+	}
+}
